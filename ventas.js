0e20b939abf1b7cfd171c2f2ab6221fbcc46aba0
unknown <amandifederico@gmail.com>
2022-07-07 19:25:01 -0300

    first

---
+++ ventas.js
@@ -0,0 +1,64 @@
+class Ventas{
+    _cliente = null;
+
+    _estado = null;
+
+    _fecha_venta = null;
+
+    _fecha_pago = null;
+
+    _fecha_entrega = null;
+
+    _productos = [];
+
+
+    constructor (fecha_venta) {
+        this.cliente = null;
+        this.fecha_venta = fecha_venta;
+        this.fecha_pago = null;
+        this.fecha_entrega = null;
+        this.productos = [];
+
+        //Object.freeze();
+        this.estado=algo;//VENDIDO
+    }
+
+    /*GETTER & SETTERS */
+    get cliente() {
+        return this._cliente;
+    }
+    set cliente(value) {
+        this._cliente = value;
+    }
+    get estado() {
+        return this._estado;
+    }
+    set estado(value) {
+        this._estado = value;
+    }
+    get fecha_venta() {
+        return this._fecha_venta;
+    }
+    set fecha_venta(value) {
+        this._fecha_venta = value;
+    }
+    get fecha_pago() {
+        return this._fecha_pago;
+    }
+    set fecha_pago(value) {
+        this._fecha_pago = value;
+    }
+    get fecha_entrega() {
+        return this._fecha_entrega;
+    }
+    set fecha_entrega(value) {
+        this._fecha_entrega = value;
+    }
+    get productos() {
+        return this._productos;
+    }
+    set productos(value) {
+        this._productos = value;
+    }
+
+}
